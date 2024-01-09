a1b1c55175552d7f11de92bff69ae985a56888b8
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-01-09 18:25:17 +0600

    Style: Add some css and add a font famaily

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 
 
-function App() {
-
-
+const App = () => {
   return (
-    <>
-       
-    </>
-  )
-}
+    <div>
+      <h3 className="text-center text-3xl">Hello World</h3>
+    </div>
+  );
+};
 
-export default App
+export default App;
